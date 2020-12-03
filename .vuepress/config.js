3b2d0dca1e59b3516814f50e5ff741c1158dce80
Sam Daitzman <sam@daitzman.com>
2020-12-03 00:01:18 -0500

    site tweaks

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,16 +1,22 @@
 module.exports = {
-    title: 'PARCEL B SEASONAL CHANGES',
-    plugins: [],
-    markdown: {
-      anchor: {
-        permalink: false,
-        permalinkBefore: true,
-        permalinkSymbol: '#'
-      },
-      extendMarkdown: md => {
-        md.use(require('@centerforopenscience/markdown-it-imsize'))
+  title: 'PARCEL B SEASONAL CHANGES',
+  plugins: [
+    [
+      "md-enhance",
+      {
+        // your options
       }
-      // lineNumbers: true
+    ]
+  ],
+  markdown: {
+    anchor: {
+      permalink: false,
+      permalinkBefore: true,
+      permalinkSymbol: '#'
+    },
+    extendMarkdown: md => {
+      md.use(require('@centerforopenscience/markdown-it-imsize'))
     }
+    // lineNumbers: true
   }
-  
+}
